744899e3751d06bbfc2869aa96017b11ef64260f
“liambrockpy” <liambrock72@gmail.com>
2023-02-03 18:46:52 +0000

    Configure session based authentication

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,14 @@ from flask_cors import CORS
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_login import LoginManager
+from flask_session import Session
 
 app = Flask(__name__)
 CORS(app)
-login = LoginManager(app)
-login.login_view = 'login'
 
 app.config.from_object(Config)
+
+server_session = Session(app)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
--- config.py
+++ config.py
@@ -1,8 +1,33 @@
+from dotenv import load_dotenv
+load_dotenv()
 import os
+import redis
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    # open new terminal and run following
+    # python -c 'import os; print(os.urandom(16))'
+    # copy the output string you get (e.g. b'_5#y2L"F4Q8z\n\xec]/')
+    # create '.env' file in root
+    # in this file add the following line
+    # SECRET_KEY= 
+    # paste your output string after the equal sign
+    SECRET_KEY = os.environ['SECRET_KEY']
+    
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
+
+    SESSION_TYPE = 'redis'
+    SESSION_PERMANENT = False
+    SESSION_USE_SIGNER = True
+
+    # download redis (or alternatively use docker - https://testdriven.io/blog/flask-server-side-sessions/)
+    # https://redis.io/docs/getting-started/installation/
+    # open new terminal and run your redis server
+    # there should be info about which url/port its running on, e.g. 127.0.0.1:6379
+    # open .env and add following line
+    # REDIS_URL='redis://127.0.0.1:6379' 
+    # edit url/port if necessary
+    SESSION_REDIS = redis.from_url(os.environ['REDIS_URL'])
