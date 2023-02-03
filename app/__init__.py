8f8c7f0eb5056c0b270dbed8292683efafb66172
“liambrockpy” <liambrock72@gmail.com>
2023-02-03 15:10:24 +0000

    First flask commit

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_cors import CORS
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+app = Flask(__name__)
+CORS(app)
+login = LoginManager(app)
+login.login_view = 'login'
+
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models
