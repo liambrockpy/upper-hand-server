9242fe66a11c101e50409bc1be69b25b65737822
Adam-Dowsett <78181424+Adam-Dowsett@users.noreply.github.com>
2023-02-06 15:40:24 +0000

    add

--- app/sockets.py
+++ app/sockets.py
@@ -1,3 +1,4 @@
+from flask import request
 from flask_socketio import SocketIO, emit, join_room, leave_room
 from app import app, db, socketio
 from app.models import Player, GameState
@@ -6,7 +7,7 @@ games = []
 
 @socketio.on('connect')
 def connect():
-    print('Client connected')
+    print(f'Client connected {request.sid}')
 
 @socketio.on('disconnect')
 def disconnect():
@@ -19,7 +20,7 @@ def handle_message(message):
 @socketio.on('create_game')
 def create_game(game_data, player_data):
     host = Player(is_host=True, name=player_data['name'], avatar = player_data['avatar'], remaining_chips=game_data['starting_chips'])
-    game = GameState(join_code=game_data['join_code'], starting_chips=game_data['starting_chips'], small_blind_amount=game_data['small_blind'], big_blind_amount=game_data['big_blind'], host=host)
+    game = GameState(join_code=game_data['join_code'], host=host)
     games.append(game)
     join_room(game.join_code)
     emit("create_game_response", game, host)
@@ -33,7 +34,7 @@ def join_game(join_code, player_data):
         player = Player(is_host=False, name=player_data['name'], avatar=player_data['avatar'])
         game.add_player(player)
         join_room(join_code)
-        emit("joined_game", game, player)
+        emit("joined_game", game, player, room=join_code)
 
 @socketio.on('leave-game')
 def leave_game(join_code, player):
@@ -43,6 +44,25 @@ def leave_game(join_code, player):
     else:
         game.remove_player(player)
         leave_room(join_code)
-        emit("left_game", game)
+        emit("left_game", game, room=join_code)
 
 @socketio.on('start-game')
+def start_game(game_data):
+    game = next((game for game in games if game["join_code"] == game_data["join_code"]), None)
+    if game == None:
+        emit('wrong_join_code')
+    else:
+        game.starting_chips = game_data["starting_chips"]
+        game.small_blind_amount = game_data["small_blind_amount"]
+        game.big_blind_amount = game_data["big_blind_amount"]
+        emit("starting-game", game, room=game_data["join_code"])    
+
+    
+@socketio.on('update_player')
+def update_player(player_data, game_data):
+    game = next((game for game in games if game["join_code"] == game_data["join_code"]), None)
+    if game == None:
+        emit('wrong_join_code')
+    else:
+        game.update_player(player_data)
+        emit("Update_Player_Response", game)
